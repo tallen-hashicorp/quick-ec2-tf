e58cc5a8963bf01f4ead995e475f7950d5e7f19b
Tyler Allen <tyler.allen@hashicorp.com>
2023-09-12 13:43:42 -0500

    Adding code

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # quick-ec2-tf
-Quick TF EC2
+Quick TF EC2 used locally by myself to spinup a quick ec2 instance for testing
+
+## Quickstart
+```bash
+doormat login
+eval $(doormat aws --account $(cat account.txt))
+terraform init
+```
+
+```bash
+terraform apply
+```
---
+++ main.tf
@@ -0,0 +1,61 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+##################################################################
+# Data sources to get VPC, subnet, security group and AMI details
+##################################################################
+data "aws_ami" "ubuntu" {
+
+    most_recent = true
+
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    }
+
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+
+    owners = ["099720109477"]
+}
+
+resource "aws_security_group" "ssh_access" {
+  name        = "ssh_access_sg"
+  description = "Security group allowing inbound SSH access"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_eip" "this" {
+  vpc      = true
+  instance = "${aws_instance.example.id}"
+}
+
+resource "aws_instance" "example" {
+  ami                         = "${data.aws_ami.ubuntu.id}"
+  instance_type               = "t2.micro"
+  vpc_security_group_ids      = ["${aws_security_group.ssh_access.id}"]
+  key_name                    = "tyler"
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "DELETE_ME"
+  }
+}
+
