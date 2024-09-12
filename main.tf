2430f5f5cea39c73cd082e2562071651291dff9d
Tyler Allen <tyler.allen@hashicorp.com>
2024-09-12 22:06:12 +0100

    Add some ports

--- main.tf
+++ main.tf
@@ -82,6 +82,27 @@ resource "aws_security_group" "vault_access" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 8220
+    to_port     = 8220
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   
   egress {
     from_port        = 0
